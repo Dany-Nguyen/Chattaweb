7ff8e9635a9c3d6c26e9e7f05392db1d3f66e36d
ahmad <ahmad.bedja-boana@etud.univ-montp2.fr>
2014-12-06 18:03:49 +0100

    les msgs de long-poll se comportent bizarement

---
+++ public/js/client.js
@@ -0,0 +1,250 @@
+(function($) {
+
+
+	var net = {
+		POLLING : "polling",
+		LONGPOLLING : "longpolling",
+		PUSH : "push"
+	}
+	var TIMEOUT_POLL = 5000;
+
+	var socket;
+	var user = {};
+	var mode;
+	var updateTimeout;
+
+	/**
+	* Connexion d'un client
+	*/ 
+
+	$('#login-form').submit(function(event){
+		event.preventDefault();
+		mode = $(this).find('input[type=radio]:checked').val();
+
+		switch(mode){
+			case net.POLLING:
+			case net.LONGPOLLING:
+				$.ajax({
+					url : 'login',
+					type : 'post',
+					data : {
+						'user': $('#pseudo').val(),
+						'mode': mode
+					},
+					cache : false,
+					success : function(res){
+						if(res.okay){
+							user = res.me;
+							logMe();
+							update();
+						}else {
+							alert("on va tous mourrir");
+						}
+					},
+					'error': function(e){
+						console.log("ca c'est mal passé\n"+e);
+					}
+				});
+				break;
+			case net.PUSH:
+				socket = io.connect('http://localhost');
+
+				user.id = $('#pseudo').val();
+				
+				////////////////////////////////////SOCKETS
+
+				socket.on('logged', function() {
+					logMe();
+				});
+
+				socket.on('newuser', function(user) {
+					addUserView(user.id)
+				});
+
+				socket.on('disconnected', function(user) {
+					$('#' + user.id).remove();
+				});
+
+				/** Affichage nouveau message */
+				socket.on('newmessage', function(message) {
+					addMessageView(message);
+				});
+
+				socket.emit('login', {
+					pseudo: user.id
+				});
+				break;
+			default:
+				alert("don't try me");
+		}
+	});
+
+	$('#message').on('keyup', function(e) {
+		var checked = $('#check-enter').is(':checked');
+		if (checked && e.which == 13 && ! e.shiftKey) {	
+			$('#chat-form').submit();
+		}
+	});
+
+
+	$('#chat-form').submit(function(event) {
+		event.preventDefault();
+
+		var msg =  $('#message').val();
+
+		switch(mode){
+			case net.POLLING:
+			case net.LONGPOLLING:
+				sendMessage(msg);
+				break;
+			case net.PUSH:
+				socket.emit('newmessage', {message: msg });		
+				break;
+			default:
+				alert("don't try my submit");
+		}
+
+		$('#message').val('');
+		$('#message').focus();
+	});
+
+	function sendMessage (message) {
+		$.ajax({
+			url : 'sendMessage',
+			type : 'post',
+			data : {
+				'user':user,
+				'message': message
+			},
+			cache : false,
+			success : function(res){
+				if(res.okay){
+					update();
+				}else {
+					alert("on va tous mourrir:sendMessage");
+				}
+			},
+			'error': function(e){
+				console.log("ca c'est mal passé\n"+e);
+			}
+		});
+	}
+
+
+	
+
+	function update() {
+		//empecher l'existence de deux appels simultanés à update
+		clearTimeout(updateTimeout);
+
+		// console.log("going to update :");
+		var addr;
+		var time;
+		if(mode === "polling"){
+			addr = 'update';
+			time = TIMEOUT_POLL;
+		}else if (mode === "longpolling"){
+			addr = 'register-lp';
+			time = 0;
+		}else {
+			alert("update: mode incorrect :"+mode);
+		}
+
+		$.ajax({
+			url : addr,
+			type : 'post',
+			data : {
+				'user': user,
+			},
+			cache : false,
+			success : function(res){
+				if(res.okay){
+					user = res.me;
+					console.log("updated successfully to version : "+ user.version);
+					updateView(res.messages,res.users);
+				}else if(res.nothing) {
+					updateUsers(res.users)
+				}else if(res.timeout) {
+					console.log("timeout LP");
+				}else{
+					alert("on va tous mourrir : update:ajax");
+				}
+			},
+			'error': function(e){
+				// console.log("update:ajax: error "+e);
+			},complete : function (argument) {
+				updateTimeout = setTimeout(update,time);
+			}
+		});
+	}
+
+
+	function logMe () {
+		$('#login').slideUp(400);
+		$('#chatchat').slideDown(400).delay(200).animate({
+			right : "50px",
+			opacity : 0,
+			"margin-top" : "20%",
+			"margin-right" : 0			
+		},{
+			duration:200,	
+			complete: function(){ $(this).fadeOut(); }
+		});
+		
+		$("body").addClass("logged");
+		$('body').prepend('<img class="chat-back" src="img/chat.png" style="opacity:0" />');
+
+		setTimeout(function() {
+			$('#chat').fadeIn(400);
+			$('#users').fadeIn(400);
+			$('#message').focus();
+			$('.chat-back').css({opacity:1});
+		}, 800);
+	}
+
+	function updateView (msgs,users) {
+		
+		for (var i = 0; i < msgs.length; i++) {
+			addMessageView(msgs[i]);
+		};
+
+		updateUsers(users);
+	}
+
+	function updateUsers (users) {
+		var oldUsers = [];
+
+		//enlever les users qui ne sont plus la (et acesoirement recuperer les uid de ce qui sont la dans)
+		$('#users .user').each(function() {
+			oldUsers.push(this.id);
+			if($.inArray( this.id, users) == -1){
+				$(this).slideUp(function(){
+					this.remove();
+				});
+			}
+		});
+		//rajouter les users que ne sont pas la
+		for (var i = users.length - 1; i >= 0; i--) {
+			if($.inArray( users[i], oldUsers) == -1) {
+				addUserView(users[i]);
+			}
+		};
+		//je pourrais fair un seul parcours (double) plutot que deux mais c'est que n²+n² environ donc ca va
+	}
+
+	function addMessageView(msg) {
+		var isme = (msg.user.id == user.id) ? "mon-message" : "";
+		$('#chat-messages').append('<li> <span class="'+isme+'"><b>' + msg.user.id + '</b> (' + msg.heure + 'h' + msg.minute + ') :</span> ' + msg.message + '</li>');
+	}
+
+	function addUserView(uid) {
+		$('#users').append('<div class="user" id="' + uid + '">' + uid + '</div>');
+	}
+
+
+
+
+	
+
+})(jQuery);
+
--- server.js
+++ server.js
@@ -1,29 +1,143 @@
-var express = require('express');
-// var bodyParser = require('body-parser')
-
+var express = require('express'); 
 var app = express();
-var port = 8080;
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
 
-var io = require('socket.io').listen(app.listen(port));
+server.listen(80);
 
-// app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-console.log('Serveur: ON');
 
-var mode = {
-	POLL : "polling",
-	LONGPOLL : "longpolling",
-	PUSH : "push"
-}
+var timeoutLP = 30000;
+Date.now = Date.now || function() { return +new Date; };  //TO SUPPORT IE < IE9  (ES5)
 
 var users = [];
 var messages = [];
 
-app.post('/login', function(req, res){
-	// var user = req.body.user;
-	// console.log(user);
-	res.send('hello world');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
+
+
+app.post('/login',function(req, res) {
+	var user = {};
+
+	user.id = req.body.user;
+	user.version = messages.length;
+	user.mode = req.body.mode;
+	user.pm = [];
+
+
+	users[user.id] = user;
+	
+	console.log("Nouvel utilisateur: "+user.id);
+
+	res.send({
+		okay:true,
+		me: user,
+		users: users.map(function (u) {
+			return u.id;
+		})
+	});	
+});
+
+
+app.post('/sendMessage', function(req, res){
+	var message = {};
+	message.message = req.body.message;
+	message.user = req.body.user;
+	date = new Date();
+	message.heure = date.getHours();
+	message.minute = date.getMinutes();
+
+	var u = req.body.user;
+	users[u.id] = u;
+
+	messages.push(message);
+
+	res.send({
+		okay:true
+	});
+});
+
+app.post('/update', function(req, res){
+
+	var u = req.body.user;
+	var msgs =[];
+
+	var isOkay = false;
+	var isNothing = false;
+
+	if(u.version < messages.length){
+		console.log("smthng to update");
+		isOkay = true;
+		
+		msgs = messages.slice(u.version,messages.length);
+		u.version = messages.length;
+		users[u.id] = u;
+		
+	}else if(u.version == messages.length){
+		isNothing = true;
+	}else{
+		console.log("error: the version is greater than the messages.length \nu.version:"+u.version+"\nmessages.length:"+messages.length);
+	}
+
+	res.send({
+		okay: isOkay,
+		nothing: isNothing,
+		me: u,
+		messages: msgs,
+		users: users
+	});
+});
+
+app.post('/register-lp', function(req, res){
+
+	// res.connection.setTimeout(0);
+
+	var u = req.body.user;
+	var oldversion = u.version;
+	
+	var lptimer = setTimeout(function() {
+		clearInterval(lpInterval);
+		res.send({
+			timeout:true
+		});
+		return;
+	},timeoutLP);
+
+	var lpInterval = setInterval(function() {
+		if(oldversion < messages.length){
+			clearTimeout(lptimer);
+			clearInterval(lpInterval);
+
+			//////////////////////////////////////// 		UPDATE COPIE COLLE, C'EST SALE
+			var u = req.body.user;
+			var msgs =[];
+			msgs = messages.slice(u.version,messages.length);
+			u.version = messages.length;
+			users[u.id] = u;
+
+			res.send({
+				okay: true,
+				me: u,
+				messages: msgs,
+				users: users
+			});
+			//////////////////////////////////////:
+		}
+	},10);
+	
+});
+
+
+
 
 /**
 * Gestion d'instance d'utilisateur par une socket
@@ -41,7 +155,7 @@ io.sockets.on('connection', function(socket) {
 	*/
 
 	socket.on('login', function(user) {
-		console.log("Nouveau utilisateur: "+JSON.stringify(user));
+		console.log("Nouvel utilisateur: "+JSON.stringify(user));
 
 		me = user;
 		me.id = user.pseudo;
@@ -56,7 +170,7 @@ io.sockets.on('connection', function(socket) {
 		}
 		delete users[me.id];
 		io.sockets.emit('disconnected',me);
-	})
+	});
 
 	/**
 	* Gestion des messages
@@ -66,6 +180,12 @@ io.sockets.on('connection', function(socket) {
 		date = new Date();
 		message.heure = date.getHours();
 		message.minute = date.getMinutes();
+
+		messages.push[message];
+		
 		io.sockets.emit('newmessage', message);
 	});
+
 });
+
+console.log('Serveur : ON');
